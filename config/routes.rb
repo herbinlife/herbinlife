7a4fed9bfe6fdbe07c4b94421d588bcfdb8fe860
herbinlife <walentina.makarenko@yandex.com>
2019-09-08 23:05:29 +0300

    install yarn. install react rails gem. added first component

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'main_page/index'
+  get 'main_page/show'
+  get 'main_page/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'main_page#index'
 end
